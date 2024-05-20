bfc3b86760c415eef8e2339ef744e6dd27e4571e
Mateusz Groblicki <mateuszgroblicki@gmail.com>
2024-05-21 01:39:33 +0200

    better cache

--- nuget/nugetclient.py
+++ nuget/nugetclient.py
@@ -1,11 +1,14 @@
+import base64
+import gzip
 import json
 import os
-from dataclasses import dataclass
-from datetime import datetime
+import time
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+from hashlib import sha256
 from typing import Any
 from urllib.parse import quote, urljoin
 from uuid import uuid4
-import gzip
 
 import jsonpickle
 import requests
@@ -16,51 +19,41 @@ from .models import main_index as main
 from .models import metadata as meta
 
 
-@dataclass(init=False)
+@dataclass()
 class CacheEntry:
-    timestamp = datetime
-    filename: str
+    keyhash: str
+    expiry_date: datetime = field(
+        default_factory=lambda: datetime.now() + timedelta(days=10))
 
     @staticmethod
-    def create() -> 'CacheEntry':
-        ce = CacheEntry()
-        ce.timestamp = datetime.now()
-        ce.filename = f'{uuid4()}.cache'
-        return ce
+    def from_filename(filename: str) -> 'CacheEntry':
+        keyhash, expiry_base64, _ = filename.split('.')
+        expiry_str = base64.b64decode(expiry_base64).decode()
+        expiry_date = datetime.fromisoformat(expiry_str)
+        return CacheEntry(keyhash, expiry_date)
 
-    @staticmethod
-    def loadjson(data: dict[str, str]) -> 'CacheEntry':
-        ce = CacheEntry()
-        ce.timestamp = datetime.fromisoformat(data['timestamp'])
-        ce.filename = data['filename']
-        return ce
-
-    def dump(self) -> dict[str, str]:
-        d = {}
-        d['timestamp'] = self.timestamp.isoformat()
-        d['filename'] = self.filename[:]
-        return d
-
-    def refresh(self) -> None:
-        self.timestamp = datetime.now()
+    @property
+    def filename(self) -> str:
+        expiry_bytes = self.expiry_date.isoformat().encode()
+        expiry_base64 = base64.b64encode(expiry_bytes).decode()
+        return f'{self.keyhash}.{expiry_base64}.cache'
 
 
 class Cache:
-    INDEX_FILENAME = 'index.json'
-
     def __init__(self, cache_dirpath: str) -> None:
         self.cache_dirpath = cache_dirpath
         self.index: dict[str, CacheEntry] = {}
-        self.index_filepath = os.path.join(cache_dirpath, Cache.INDEX_FILENAME)
+
+    @staticmethod
+    def hashkey(key: str) -> str:
+        return sha256(key.encode()).hexdigest()
 
     @staticmethod
     def init(cache_dirpath: str) -> 'Cache':
         def load_index(cache: Cache) -> None:
-            if os.path.exists(cache.index_filepath) and os.path.isfile(cache.index_filepath):
-                with open(cache.index_filepath, mode='rt') as file:
-                    data: dict = json.load(file)
-                    for key in data.keys():
-                        cache.index[key] = CacheEntry.loadjson(data[key])
+            for filename in os.listdir(cache_dirpath):
+                ce = CacheEntry.from_filename(filename)
+                cache.index[ce.keyhash] = ce
 
         if not os.path.exists(cache_dirpath):
             os.mkdir(cache_dirpath)
@@ -69,38 +62,31 @@ class Cache:
         load_index(cache)
         return cache
 
-    def save(self) -> None:
-        with globals.lock:
-            index_dict: dict[str, dict[str, str]] = {}
-            for key in self.index.keys():
-                index_dict[key] = self.index[key].dump()
-
-            with open(self.index_filepath, mode='wt') as file:
-                json.dump(index_dict, file)
-
     def add(self, key: str, value: Any) -> None:
+        keyhash = Cache.hashkey(key)
+
         with globals.lock:
-            cache_entry: CacheEntry = None
-            if ce := self.index.get(key):
-                ce.refresh()
-                cache_entry = ce
-            if not cache_entry:
-                cache_entry = CacheEntry.create()
-                self.index[key] = cache_entry
+            if ce := self.index.get(keyhash):
+                del self.index[keyhash]
+                filepath = os.path.join(self.cache_dirpath, ce.filename)
+                os.remove(filepath)
+
+            cache_entry = CacheEntry(keyhash)
+            self.index[cache_entry.keyhash] = cache_entry
 
             filepath = os.path.join(self.cache_dirpath, cache_entry.filename)
             with gzip.open(filepath, mode='wb') as file:
                 json: str = jsonpickle.encode(value)
                 file.write(json.encode())
 
-            self.save()
-
     def get(self, key: str) -> Any | None:
+        keyhash = Cache.hashkey(key)
+
         with globals.lock:
-            if key not in self.index:
+            if keyhash not in self.index:
                 return None
             filepath = os.path.join(
-                self.cache_dirpath, self.index[key].filename)
+                self.cache_dirpath, self.index[keyhash].filename)
             with gzip.open(filepath, mode='rb') as file:
                 json = file.read().decode()
                 value = jsonpickle.decode(json)
